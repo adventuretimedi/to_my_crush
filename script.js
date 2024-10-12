feb3b45e7f4c45bd54cad25e801ad630531ba1e8
Diana Peretiatko <dperetiatko@gen2dev.com>
2024-10-12 19:31:03 +0300

    adding new styles

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
-const question = document.querySelector(".question");
+const answer = document.querySelector(".answer");
 const gifContainer = document.querySelector(".gif");
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Got you! Now you're infected with my love 😘";
+  answer.innerHTML = "Got you! Now you're infected with my love 😘";
 
-  // Очищення контейнера
-  gifContainer.innerHTML = ""; // Очищаємо контейнер для гіфки
+  gifContainer.innerHTML = "";
 
-  // Додавання гіфки з Tenor
   const gifEmbed = document.createElement("div");
   gifEmbed.className = "tenor-gif-embed";
   gifEmbed.setAttribute("data-postid", "11397231996208728070");
@@ -18,16 +16,14 @@ yesBtn.addEventListener("click", () => {
   gifEmbed.setAttribute("data-aspect-ratio", "1.09211");
   gifEmbed.setAttribute("data-width", "100%");
 
-  // Додаємо гіфку в контейнер
   gifContainer.appendChild(gifEmbed);
 
-  // Додаємо скрипт Tenor для вбудовування
   const script = document.createElement("script");
   script.type = "text/javascript";
   script.async = true;
   script.src = "https://tenor.com/embed.js";
 
-  gifContainer.appendChild(script); // Додаємо скрипт в контейнер
+  gifContainer.appendChild(script);
 });
 
 // Make the No button move randomly on hover
