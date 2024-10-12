86fb3f4eb67e90849f9e1f181e786f4339f3df5f
Diana Peretiatko <dperetiatko@gen2dev.com>
2024-10-12 23:31:00 +0300

    final

--- script.js
+++ script.js
@@ -3,62 +3,70 @@ const noBtn = document.querySelector(".no-btn");
 const answer = document.querySelector(".answer");
 const gifContainer = document.querySelector(".gif");
 
-// Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
-  answer.innerHTML = "Got you! Now you're infected with my love 😘";
+    answer.innerHTML = "Got you! Now you're infected with my love 😘";
 
-  gifContainer.innerHTML = "";
+    gifContainer.innerHTML = "";
 
-  const gifEmbed = document.createElement("div");
-  gifEmbed.className = "tenor-gif-embed";
-  gifEmbed.setAttribute("data-postid", "11397231996208728070");
-  gifEmbed.setAttribute("data-share-method", "host");
-  gifEmbed.setAttribute("data-aspect-ratio", "1.09211");
-  gifEmbed.setAttribute("data-width", "100%");
+    const gifEmbed = document.createElement("div");
+    gifEmbed.className = "tenor-gif-embed";
+    gifEmbed.setAttribute("data-postid", "11397231996208728070");
+    gifEmbed.setAttribute("data-share-method", "host");
+    gifEmbed.setAttribute("data-aspect-ratio", "1.09211");
+    gifEmbed.setAttribute("data-width", "100%");
 
-  gifContainer.appendChild(gifEmbed);
+    gifContainer.appendChild(gifEmbed);
 
-  const script = document.createElement("script");
-  script.type = "text/javascript";
-  script.async = true;
-  script.src = "https://tenor.com/embed.js";
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.async = true;
+    script.src = "https://tenor.com/embed.js";
 
-  gifContainer.appendChild(script);
+    gifContainer.appendChild(script);
+    
+    for (let i = 0; i < 10; i++) {
+        createHeart();
+    }
 });
 
-// Make the No button move randomly on hover
-noBtn.addEventListener("touchstart", () => {
-  const wrapper = document.querySelector(".wrapper");
-  const wrapperRect = wrapper.getBoundingClientRect();
-  const noBtnRect = noBtn.getBoundingClientRect();
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.className = "heart";
 
-  // Calculate max positions to ensure the button stays within the wrapper
-  const maxX = wrapperRect.width - noBtnRect.width;
-  const maxY = wrapperRect.height - noBtnRect.height;
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.bottom = "0";
 
-  const randomX = Math.floor(Math.random() * maxX);
-  const randomY = Math.floor(Math.random() * maxY);
+  heart.style.animationDelay = Math.random() * 2 + "s";
 
-  noBtn.style.left = randomX + "px";
-  noBtn.style.top = randomY + "px";
+  document.body.appendChild(heart);
+
+  heart.addEventListener("animationend", () => {
+      heart.remove();
+  });
+}
+
+
+const moveButton = () => {
+    const wrapper = document.querySelector(".wrapper");
+    const wrapperRect = wrapper.getBoundingClientRect();
+    const noBtnRect = noBtn.getBoundingClientRect();
+
+    const maxX = wrapperRect.width - noBtnRect.width;
+    const maxY = wrapperRect.height - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.style.position = 'absolute';
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+};
+
+noBtn.addEventListener("mouseover", moveButton);
+noBtn.addEventListener("touchstart", moveButton);
+document.addEventListener("mousemove", (e) => {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    if (e.clientX < noBtnRect.left || e.clientX > noBtnRect.right || e.clientY < noBtnRect.top || e.clientY > noBtnRect.bottom) {
+        moveButton();
+    }
 });
-
-
-// noBtn.addEventListener("mouseover", moveButton);
-// noBtn.addEventListener("touchstart", moveButton); // Додаємо підтримку для сенсорних пристроїв
-
-// function moveButton() {
-//   const wrapper = document.querySelector(".wrapper");
-//   const wrapperRect = wrapper.getBoundingClientRect();
-//   const noBtnRect = noBtn.getBoundingClientRect();
-
-//   // Розрахунок максимальних позицій, щоб кнопка залишалася в межах обгортки
-//   const maxX = wrapperRect.width - noBtnRect.width;
-//   const maxY = wrapperRect.height - noBtnRect.height;
-
-//   const randomX = Math.floor(Math.random() * maxX);
-//   const randomY = Math.floor(Math.random() * maxY);
-
-//   noBtn.style.left = randomX + "px";
-//   noBtn.style.top = randomY + "px";
-// }
